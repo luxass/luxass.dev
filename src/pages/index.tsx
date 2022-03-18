a9d3974db5b1451b32c189f8b8c121f2059ba266
DeprecatedLuxas <lucasmathias65@icloud.com>
2022-03-18 10:48:28 +0100

    small changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,5 +4,5 @@ import { useMediaQuery } from "@hooks/use-mediaquery";
 export default function Home() {
   const isMobile = useMediaQuery("only screen and (max-width: 768px)");
 
-  return <Layout image={""}>Hello</Layout>;
+  return <Layout image={""}>test</Layout>;
 }
