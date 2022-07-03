efb419f51b76515d08ffbc844a49ea2abe15b6d3
luxass <lucasnrgaard@gmail.com>
2022-07-03 11:32:36 +0200

    chore: testing isr

---
+++ src/pages/projects/[id].tsx
@@ -0,0 +1,10 @@
+import { ProjectLayout } from "@layouts/project";
+
+function Project() {
+  return <div></div>;
+}
+
+// eslint-disable-next-line react/display-name
+Project.Layout = ProjectLayout;
+
+export default Project;
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,10 +1,21 @@
 import { DefaultLayout } from "@layouts/default";
+import { InferGetStaticPropsType } from "next/types";
+import { useEffect, useState } from "react";
 
-function Projects() {
-  return <div></div>;
+function Projects({ title }: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div className="text-white">{title}</div>;
 }
 
 // eslint-disable-next-line react/display-name
 Projects.Layout = DefaultLayout;
+
+export async function getStaticProps() {
+  return {
+    props: {
+      title: new Date().getTime(),
+    },
+    revalidate: 60,
+  };
+}
 
 export default Projects;
