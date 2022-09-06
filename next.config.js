0308aecddd2d732fb9efeedf27b2e2c55ca3cedb
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-09-06 11:10:16 +0200

    is vercel broken

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer(
   withContentlayer({
-    swcMinify: true,
     reactStrictMode: true,
     experimental: {
       legacyBrowsers: false,
