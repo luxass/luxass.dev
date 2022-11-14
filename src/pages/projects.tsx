f9ee251737b38d27781e630c02996e50f656dc7e
Lucas Nørgård <lucasnrgaard@gmail.com>
2022-11-14 12:10:33 +0100

    fixed styling on projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -27,21 +27,23 @@ export const getStaticProps: GetStaticProps = async () => {
 const ProjectsPage: NextPage<{ projects: Project[] }> = ({ projects }) => {
   return (
     <DefaultLayout title="Lucas Norgaard - Projects">
-      <section className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
-        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
-          Projects
-        </h1>
-        <p className="mb-4 text-gray-600 dark:text-gray-400">
-          These are some of my projects, I&apos;ve been working on.
-        </p>
-      </section>
+      <div className="p-3">
+        <section className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
+          <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
+            Projects
+          </h1>
+          <p className="mb-4 text-gray-600 dark:text-gray-400">
+            These are some of my projects, I&apos;ve been working on.
+          </p>
+        </section>
 
-      <section className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
-        {projects &&
-          projects.map((project) => (
-            <ProjectCard key={project.url} project={project} />
-          ))}
-      </section>
+        <section className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+          {projects &&
+            projects.map((project) => (
+              <ProjectCard key={project.url} project={project} />
+            ))}
+        </section>
+      </div>
     </DefaultLayout>
   );
 };
