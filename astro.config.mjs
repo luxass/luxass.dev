4ceca80c45319f73d2efd06662e79f6b6c9c4379
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-01-25 16:55:11 +0100

    chore: added import-cost file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import unocss from "unocss/astro";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://luxass.dev",
-  integrations: [unocss(), mdx(), sitemap(), prefetch()],
+  integrations: [unocss(), sitemap(), prefetch()]
 });
