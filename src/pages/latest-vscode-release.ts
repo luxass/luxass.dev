d931c4d5e685999083b0b808f234d2ba4a9e5f4d
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-07-13 17:51:39 +0200

    fix: long taking build

--- src/pages/latest-vscode-release.ts
+++ src/pages/latest-vscode-release.ts
@@ -1,21 +1,18 @@
-import { octokit } from "../utils/octokit";
-
 export async function get() {
-  if (!import.meta.env.GITHUB_TOKEN) {
-    throw new Error("GITHUB_TOKEN environment variable is not set");
-  }
-  const releases = await octokit.paginate("GET /repos/{owner}/{repo}/releases", {
-    owner: "microsoft",
-    repo: "vscode",
-    per_page: 1
-  });
+  const data = await fetch("https://api.github.com/repos/microsoft/vscode/releases?per_page=1", {
+    headers: {
+      "Authorization": `bearer ${import.meta.env.GITHUB_TOKEN}`,
+      "Content-Type": "application/json"
+    }
+  }).then((res) => res.json());
 
-  if (!releases) {
-    throw new Error("No releases found");
+  if (!data) {
+    throw new Error("No data found");
   }
 
 
-  const release = releases[0];
+
+  const release = data[0];
 
   if (!("tag_name" in release)) {
     throw new Error("No tag_name found");
