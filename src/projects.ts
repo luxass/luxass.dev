c00b7526bd32b957d816ff2a08dd77e7a0d399de
luxass <lucasnrgaard@gmail.com>
2023-09-03 01:40:35 +0000

    chore: updated projects

--- src/projects.ts
+++ src/projects.ts
@@ -2,146 +2,146 @@
 
 export const projects = [
   {
-    name: "luxass.dev",
-    owner: "luxass",
-    description: "No description was provided.",
-    url: "https://github.com/luxass/luxass.dev",
-    pushedAt: "2023-09-02T14:36:00Z",
-    language: {
-      color: "#ff5a03",
-      name: "Astro",
-    },
+    "name": "luxass.dev",
+    "owner": "luxass",
+    "description": "No description was provided.",
+    "url": "https://github.com/luxass/luxass.dev",
+    "pushedAt": "2023-09-02T14:42:44Z",
+    "language": {
+      "color": "#ff5a03",
+      "name": "Astro"
+    }
   },
   {
-    name: "cdn-resolve",
-    owner: "luxass",
-    description: "Resolves packages to their relative CDN URLS",
-    url: "https://github.com/luxass/cdn-resolve",
-    pushedAt: "2023-09-02T02:34:01Z",
-    language: {
-      color: "#3178c6",
-      name: "TypeScript",
-    },
+    "name": "cdn-resolve",
+    "owner": "luxass",
+    "description": "Resolves packages to their relative CDN URLS",
+    "url": "https://github.com/luxass/cdn-resolve",
+    "pushedAt": "2023-09-02T02:34:01Z",
+    "language": {
+      "color": "#3178c6",
+      "name": "TypeScript"
+    }
   },
   {
-    name: "load-config",
-    owner: "luxass",
-    description: "No description was provided.",
-    url: "https://github.com/luxass/load-config",
-    pushedAt: "2023-09-02T02:23:39Z",
-    language: {
-      color: "#3178c6",
-      name: "TypeScript",
-    },
+    "name": "load-config",
+    "owner": "luxass",
+    "description": "No description was provided.",
+    "url": "https://github.com/luxass/load-config",
+    "pushedAt": "2023-09-02T02:23:39Z",
+    "language": {
+      "color": "#3178c6",
+      "name": "TypeScript"
+    }
   },
   {
-    name: "neoman",
-    owner: "luxass",
-    description: "No description was provided.",
-    url: "https://github.com/luxass/neoman",
-    pushedAt: "2023-09-02T00:52:10Z",
-    language: {
-      color: "#3178c6",
-      name: "TypeScript",
-    },
+    "name": "neoman",
+    "owner": "luxass",
+    "description": "No description was provided.",
+    "url": "https://github.com/luxass/neoman",
+    "pushedAt": "2023-09-02T00:52:10Z",
+    "language": {
+      "color": "#3178c6",
+      "name": "TypeScript"
+    }
   },
   {
-    name: "eslint-config",
-    owner: "luxass",
-    description: "My shareable ESLint config",
-    url: "https://github.com/luxass/eslint-config",
-    pushedAt: "2023-09-01T23:23:25Z",
-    language: {
-      color: "#f1e05a",
-      name: "JavaScript",
-    },
+    "name": "eslint-config",
+    "owner": "luxass",
+    "description": "My shareable ESLint config",
+    "url": "https://github.com/luxass/eslint-config",
+    "pushedAt": "2023-09-01T23:23:25Z",
+    "language": {
+      "color": "#f1e05a",
+      "name": "JavaScript"
+    }
   },
   {
-    name: "tsup-problem-matchers",
-    owner: "luxass",
-    description: "Problem matchers for tsup tasks in VSCode",
-    url: "https://github.com/luxass/tsup-problem-matchers",
-    pushedAt: "2023-09-01T23:12:56Z",
-    language: {
-      color: "#f1e05a",
-      name: "JavaScript",
-    },
+    "name": "tsup-problem-matchers",
+    "owner": "luxass",
+    "description": "Problem matchers for tsup tasks in VSCode",
+    "url": "https://github.com/luxass/tsup-problem-matchers",
+    "pushedAt": "2023-09-01T23:12:56Z",
+    "language": {
+      "color": "#f1e05a",
+      "name": "JavaScript"
+    }
   },
   {
-    name: "esbuild-cdn-imports",
-    owner: "luxass",
-    description: "This plugin intercepts imports and resolves them to a CDN URL.",
-    url: "https://github.com/luxass/esbuild-cdn-imports",
-    pushedAt: "2023-09-01T22:57:25Z",
-    language: {
-      color: "#3178c6",
-      name: "TypeScript",
-    },
+    "name": "esbuild-cdn-imports",
+    "owner": "luxass",
+    "description": "This plugin intercepts imports and resolves them to a CDN URL.",
+    "url": "https://github.com/luxass/esbuild-cdn-imports",
+    "pushedAt": "2023-09-01T22:57:25Z",
+    "language": {
+      "color": "#3178c6",
+      "name": "TypeScript"
+    }
   },
   {
-    name: "jsonc-parse",
-    owner: "luxass",
-    description: "A lightweight JSON with Comments parser.",
-    url: "https://github.com/luxass/jsonc-parse",
-    pushedAt: "2023-09-01T22:50:26Z",
-    language: {
-      color: "#3178c6",
-      name: "TypeScript",
-    },
+    "name": "jsonc-parse",
+    "owner": "luxass",
+    "description": "A lightweight JSON with Comments parser.",
+    "url": "https://github.com/luxass/jsonc-parse",
+    "pushedAt": "2023-09-01T22:50:26Z",
+    "language": {
+      "color": "#3178c6",
+      "name": "TypeScript"
+    }
   },
   {
-    name: "tsconf-utils",
-    owner: "luxass",
-    description: "Utilities for working with tsconfig.json files",
-    url: "https://github.com/luxass/tsconf-utils",
-    pushedAt: "2023-09-01T22:26:14Z",
-    language: {
-      color: "#3178c6",
-      name: "TypeScript",
-    },
+    "name": "tsconf-utils",
+    "owner": "luxass",
+    "description": "Utilities for working with tsconfig.json files",
+    "url": "https://github.com/luxass/tsconf-utils",
+    "pushedAt": "2023-09-01T22:26:14Z",
+    "language": {
+      "color": "#3178c6",
+      "name": "TypeScript"
+    }
   },
   {
-    name: "elysius",
-    owner: "luxass",
-    description: "Find a file or directory by traversing",
-    url: "https://github.com/luxass/elysius",
-    pushedAt: "2023-08-10T19:48:20Z",
-    language: {
-      color: "#3178c6",
-      name: "TypeScript",
-    },
+    "name": "elysius",
+    "owner": "luxass",
+    "description": "Find a file or directory by traversing",
+    "url": "https://github.com/luxass/elysius",
+    "pushedAt": "2023-08-10T19:48:20Z",
+    "language": {
+      "color": "#3178c6",
+      "name": "TypeScript"
+    }
   },
   {
-    name: "prettier-config",
-    owner: "luxass",
-    description: "No description was provided.",
-    url: "https://github.com/luxass/prettier-config",
-    pushedAt: "2023-06-20T15:03:41Z",
-    language: {
-      color: "#f1e05a",
-      name: "JavaScript",
-    },
+    "name": "prettier-config",
+    "owner": "luxass",
+    "description": "No description was provided.",
+    "url": "https://github.com/luxass/prettier-config",
+    "pushedAt": "2023-06-20T15:03:41Z",
+    "language": {
+      "color": "#f1e05a",
+      "name": "JavaScript"
+    }
   },
   {
-    name: "typeschema",
-    owner: "luxass",
-    description: "Generate JSON Schemas, Zod Schemas from TypeScript types.",
-    url: "https://github.com/luxass/typeschema",
-    pushedAt: "2023-06-20T14:53:59Z",
-    language: {
-      color: "#3178c6",
-      name: "TypeScript",
-    },
+    "name": "typeschema",
+    "owner": "luxass",
+    "description": "Generate JSON Schemas, Zod Schemas from TypeScript types.",
+    "url": "https://github.com/luxass/typeschema",
+    "pushedAt": "2023-06-20T14:53:59Z",
+    "language": {
+      "color": "#3178c6",
+      "name": "TypeScript"
+    }
   },
   {
-    name: "action-trending",
-    owner: "luxass",
-    description: "No description was provided.",
-    url: "https://github.com/luxass/action-trending",
-    pushedAt: "2023-06-01T06:03:06Z",
-    language: {
-      color: "#3178c6",
-      name: "TypeScript",
-    },
-  },
+    "name": "action-trending",
+    "owner": "luxass",
+    "description": "No description was provided.",
+    "url": "https://github.com/luxass/action-trending",
+    "pushedAt": "2023-06-01T06:03:06Z",
+    "language": {
+      "color": "#3178c6",
+      "name": "TypeScript"
+    }
+  }
 ];
