d76a43d9edb98a5d0c29f387b05572baea09cef0
luxass <lucasnrgaard@gmail.com>
2023-09-12 01:39:33 +0000

    chore: updated projects

--- src/projects.ts
+++ src/projects.ts
@@ -6,7 +6,7 @@ export const projects = [
     "owner": "luxass",
     "description": "No description was provided.",
     "url": "https://github.com/luxass/luxass.dev",
-    "pushedAt": "2023-09-10T19:54:24Z",
+    "pushedAt": "2023-09-11T11:18:33Z",
     "language": {
       "color": "#ff5a03",
       "name": "Astro"
