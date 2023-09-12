fbbeee1b2815a0af5c8724c101e54be042c1a64e
luxass <lucasnrgaard@gmail.com>
2023-09-12 14:51:57 +0200

    testing vercdel adapter again

--- astro.config.ts
+++ astro.config.ts
@@ -6,22 +6,21 @@ import mdx from "@astrojs/mdx";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import remarkSmartypants from "remark-smartypants";
+import AutoImport from "unplugin-auto-import/astro";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://luxass.dev",
-  integrations: [
-    unocss({
-      injectReset: true,
-    }),
-    sitemap({
-      filter(page) {
-        return !page.includes("$");
-      },
-    }),
-    solidJs(),
-    mdx(),
-  ],
+  integrations: [unocss({
+    injectReset: true,
+  }), sitemap({
+    filter(page) {
+      return !page.includes("$");
+    },
+  }), solidJs(), mdx(), AutoImport({
+    dts: true,
+  })],
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
@@ -33,4 +32,10 @@ export default defineConfig({
     }]],
     rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
   },
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+    edgeMiddleware: true,
+    functionPerRoute: false,
+  }),
 });
