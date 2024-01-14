80a1c84f97b15dce16bf4e36bfed0f735415775d
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-01-14 20:22:57 +0100

    feat: add icon to projects frontmatter

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,12 +17,13 @@ const projects = defineCollection({
   schema: z.object({
     handle: z.string().optional(),
     githubUrl: z.string(),
-    // github repo names is a maximum of 100 characters.
-    name: z.string().max(100),
+    // github repo names is a maximum of 100 characters, but why do we need that much?
+    name: z.string().max(30),
     owner: z.string(),
     description: z.string().max(120).nullable(),
     npm: z.string().optional(),
     handles: z.array(z.string()).optional(),
+    icon: z.string().optional(),
   }),
 });
 
