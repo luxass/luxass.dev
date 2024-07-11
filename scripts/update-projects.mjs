f686d1e9376193063de56491dfcca949c8eb0d0e
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-11 05:05:47 +0200

    chore: Update core.setOutput in update-projects.mjs

--- scripts/update-projects.mjs
+++ scripts/update-projects.mjs
@@ -251,7 +251,7 @@ export async function run(ctx) {
     }
 
     core.setOutput("branchName", branchName);
-    core.setOutput("success", updateBranchExists ? "true" : "false");
+    core.setOutput("success", "true");
   } catch (/** @type {any} */ err) {
     core.setFailed(err.message);
   }
