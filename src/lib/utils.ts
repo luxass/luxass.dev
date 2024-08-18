318f172a655048eff4c93fa11ae58bf60e08bff1
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-08-18 09:35:59 +0200

    chore: remove unused deps

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,14 +5,3 @@ export function sortPosts(
 ): CollectionEntry<"posts">[] {
   return posts.sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
 }
-
-const GLOBAL_TEXT_DECODER = new TextDecoder();
-
-export function base64ToString(str: string): string {
-  const byteArray = new Uint8Array(
-    atob(str)
-      .split("")
-      .map((char) => char.charCodeAt(0)),
-  );
-  return GLOBAL_TEXT_DECODER.decode(byteArray);
-}
