d721496f633ac393912bcac780a20fe0e10bae32
Lucas <lucasnrgaard@gmail.com>
2025-05-17 20:07:33 +0200

    chore: add debug logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
   const url = new URL(context.request.url);
+  // eslint-disable-next-line no-console
+  console.log("url", url);
 
   // if we hit `spectral-ruleset.luxass.dev` we will redirect to unpkg
   if (url.hostname.startsWith("spectral-ruleset")) {
