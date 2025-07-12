f24ec0bef7618dd19b50b2edb801d6c191993480
Lucas <lucasnrgaard@gmail.com>
2025-07-12 08:39:15 +0200

    feat: add CVLink component
    - Introduced a new `CVLink` component for displaying links with icons.
    - The component accepts `href`, `icon`, `label`, and optional `className` props.
    - Utilizes `stripProtocol` utility to format the displayed URL.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,9 @@ export function sortPosts(
 ): CollectionEntry<"posts">[] {
   return posts.sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
 }
+
+export function stripProtocol(url: string): string {
+  const parsedUrl = new URL(url);
+
+  return parsedUrl.hostname + parsedUrl.pathname + parsedUrl.search;
+}
